dd76cb6e542c84a6acfbd72a25204714a981d839
HaeRyeong Jin <jhr5559@gmail.com>
2026-03-31 21:30:07 +0900

    fix: upload API URL 수정

--- src/api/fileApi.ts
+++ src/api/fileApi.ts
@@ -13,7 +13,7 @@ export const apiFileUpload = async (file: File, category: ImageCategory) => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const response = await axiosAuthInstance.post(`/files/upload/${category}`, formData, {
+    const response = await axiosAuthInstance.post(`/files/${category}`, formData, {
         headers: { "Content-Type": "multipart/form-data" }
     });
 
